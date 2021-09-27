e25091c80be64dc2f5895b42b9d91fefd661542b
alexx sub <alexei.s@list.ru>
2021-09-27 20:35:27 +0300

    works express

---
+++ server/src/server.ts
@@ -0,0 +1,12 @@
+process.env['NODE_CONFIG_DIR'] = __dirname + '/configs'
+
+import 'dotenv/config'
+import App from 'app'
+import IndexRoute from '@routes/index.route'
+import validateEnv from '@utils/validateEnv'
+
+validateEnv()
+
+const app = new App([new IndexRoute()])
+
+app.listen();
